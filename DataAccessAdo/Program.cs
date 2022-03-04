5caa83d293bf69bf218cdb0631204fefa38407e6
Francisco ROSALES <rosalesf@sancristobal.com.ar>
2022-03-04 18:08:31 -0300

    Improve code

--- DataAccessAdo/Program.cs
+++ DataAccessAdo/Program.cs
@@ -4,10 +4,7 @@ using DataAccessAdo.Infrastructures;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,7 +18,6 @@ builder.Host.ConfigureContainer<ContainerBuilder>(s =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- DataAccessAdo/Repositories/SkaterRepository.cs
+++ DataAccessAdo/Repositories/SkaterRepository.cs
@@ -2,33 +2,32 @@
 using DataAccessAdo.Domains;
 using MySqlConnector;
 
-namespace DataAccessAdo.Repositories
+namespace DataAccessAdo.Repositories;
+
+public class SkaterRepository : RepositoryBase, ISkaterRepository
 {
-    public class SkaterRepository : RepositoryBase, ISkaterRepository
+    public SkaterRepository(MySqlConnection connection) : base(connection)
     {
-        public SkaterRepository(MySqlConnection connection) : base(connection)
+    }
+
+    public IEnumerable<Skater> GetAll() {
+        var result = new List<Skater>();
+        const string sqlString = "SELECT id, name, brand FROM Skaters";
+
+        using var connection = GetConnection();
+        connection.Open();
+        using var command = new MySqlCommand(sqlString, connection);
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
         {
-        }
-
-        public IEnumerable<Skater> GetAll() {
-            var result = new List<Skater>();
-            const string sqlString = "SELECT id, name, brand FROM Skaters";
-
-            using var connection = GetConnection();
-            connection.Open();
-            using var command = new MySqlCommand(sqlString, connection);
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            result.Add(new Skater()
             {
-                result.Add(new Skater()
-                {
-                    Id = reader.GetInt32(0),
-                    Name = reader.GetString(1),
-                    Brand = reader.GetString(2),
-                });
-            }
-
-            return result;
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Brand = reader.GetString(2),
+            });
         }
+
+        return result;
     }
 }
